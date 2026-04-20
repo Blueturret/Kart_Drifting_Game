d26fe02f5a7493780add7d2003c68b116391194c
Patrick Afram <patrick06.afram@gmail.com>
2026-04-20 11:41:58 +0200

    Added car physics and forward/backward movement

---
+++ Scripts/wheel.gd
@@ -0,0 +1,16 @@
+extends RayCast3D
+
+# Class for a physics based wheel
+class_name RaycastWheel
+
+@export_category("Wheel properties")
+
+@export var springStrength := 100.0
+@export var springDamping := 2.0
+@export var restDistance := 0.5
+@export var overExtend := 0.2
+
+@export var isMotor := false
+
+@onready var wheel : Node3D = get_child(0)
+@export var wheelRadius := 0.4
